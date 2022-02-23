f162bfb58299ca1f19ce30ad185762da23f71205
yanka <yanka@ZYZF>
2022-02-23 19:39:57 +0300

    Добавьте файлы проекта.

---
+++ L12_1 Kalyshev/L12_1 Kalyshev.cpp
@@ -0,0 +1,46 @@
+﻿// L12_1 Kalyshev.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+struct Node* first = NULL;
+
+void printList() {
+	struct Node* ptr = first;
+	while (ptr != NULL) {
+		printf("(%d) -> ", ptr->data);
+		ptr = ptr->next;
+	}
+	printf("NULL\n");
+}
+
+void main() {
+	printList();
+
+	struct Node node1 = { 1, NULL };
+	struct Node node2 = { 2, NULL };
+	struct Node node3 = { 3, NULL };
+	printList();
+
+	first = &node1;
+	printList();
+
+	node1.next = &node2;
+	printList();
+
+	node2.next = &node3;
+	printList();
+
+	{
+		int x;
+		scanf("%d", &x);
+	}
+}
---
+++ L12_2 Kalyshev/L12_2 Kalyshev.cpp
@@ -0,0 +1,69 @@
+﻿// L12_2 Kalyshev.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+struct Node* first = NULL;
+
+void printList() {
+	struct Node* ptr = first;
+	while (ptr != NULL) {
+		printf("(%d) -> ", ptr->data);
+		ptr = ptr->next;
+	}
+	printf("NULL\n");
+}
+
+void addToHead(int value) {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+	newNode->next = first;
+	newNode->data = value;
+
+	first = newNode;
+}
+
+int deleteFromHead() {
+	int value = first->data;
+	struct Node* delNode = first;
+
+	first = first->next;
+	free(delNode);
+
+	return value;
+}
+
+
+void main() {
+	printList();
+
+	addToHead(10);
+	printList();
+
+	addToHead(20);
+	printList();
+
+	addToHead(30);
+	printList();
+
+	int x1 = deleteFromHead();
+	printf("x1 = %d\n", x1);
+	printList();
+
+	int x2 = deleteFromHead();
+	printf("x2 = %d\n", x2);
+	printList();
+
+	{
+		int x;
+		scanf("%d", &x);
+	}
+}
